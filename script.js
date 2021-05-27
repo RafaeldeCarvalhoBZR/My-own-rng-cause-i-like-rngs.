b078ba3f15cab6dba5b40d18136452d50eecc4c7
RafaeldeCarvalhoBZR <84476936+RafaeldeCarvalhoBZR@users.noreply.github.com>
2021-05-27 04:04:31 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
+let n = parseInt(prompt("How many numbers are to be generated?")); 
 let x = parseInt(prompt("Min range value?"));
 let y = parseInt(prompt("Max range Value?"));
-
-function rng(min, max) {
-     return Math.floor(Math.random() * (max + 1 - min) + min );
+let ar = [];
+for (let index = 0; index < n; index++) {
+    function rng(min, max) {
+        return Math.floor(Math.random() * (max + 1 - min) + min );
+    }
+    ar.push(rng(x, y));
 }
-console.log(rng(x, y));
+console.log(ar);
